4fd922d60d1039c6368ca2cf838ea15dd5d6dad1
baolongnguyenmac <baolongnguyen.mac@gmail.com>
2025-06-06 11:04:54 +0700

    install docker

--- cui.sh
+++ cui.sh
@@ -1,7 +1,53 @@
+# install all the stuff below at once
+# never test them
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 echo "============= Install curl ============="
 sudo apt install curl
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+echo "============= Setup Zsh ============="
+
+# install zsh and set default shell
+sudo apt install zsh
+chsh -s $(which zsh)
+# echo "zsh" >> ~/.bashrc
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# plugins for oh-my-zsh
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+echo "============= Setup Docker ============="
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker run hello-world
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 echo "============= Setup Conda ============="
+
 # install
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
@@ -12,57 +58,65 @@ rm ~/miniconda3/miniconda.sh
 source ~/miniconda3/bin/activate
 conda init --all
 
-echo "============= Setup Zsh ============="
-# install zsh and set default shell
-sudo apt install zsh
-# chsh -s $(which zsh)
-echo "zsh" >> ~/.bashrc
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# plugins for oh-my-zsh
-sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# install uv and setup venv
 echo "============= Setup UV ============="
+
 curl -LsSf https://astral.sh/uv/install.sh | sh
 # uv venv ~/.nblong --python 3.11
 # echo "source ~/.nblong/bin/activate" >> ~/.zshrc
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 echo "============= Install iBus ============="
+
 sudo add-apt-repository ppa:ubuntu-vn/ppa
 sudo apt-get update
 sudo apt-get install ibus-unikey
 ibus restart
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 # prevent laptop from sleeping
 echo "============= Install caffeine ============="
 sudo apt install caffeine
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 echo "============= Install tree ============="
 sudo apt install tree
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # save battery life
 echo "============= Install tlp ============="
 sudo apt install tlp
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 # check basic info of laptop
 echo "============= Install neofetch ============="
 sudo apt install neofetch
 
-# install snap to install other package provided by amazon :D
-echo "============= Install Snap ============="
-sudo apt install snapd
-sudo snap install snapd # get the latest one
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# install typst using snap to be pdf engine of pandoc
-echo "============= Install Typst ============="
-sudo snap install typst
+# # install snap to install other package provided by amazon :D
+# echo "============= Install Snap ============="
+# sudo apt install snapd
+# sudo snap install snapd # get the latest one
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# # install typst using snap to be pdf engine of pandoc
+# echo "============= Install Typst ============="
+# sudo snap install typst
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # install latex: the last one (since we have to download 6GB)
 echo "============= Install Latex ============="
 sudo apt install texlive-full
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # at this time, you should restart the comp.
